32b9d8a3e4a068944cce75d9542dd62956b6510b
SamarMatoussi <148507480+SamarMatoussi@users.noreply.github.com>
2024-11-19 20:46:14 +0100

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,58 +3,93 @@ pipelineJob('pipeline') {
     cps {
       script(
 '''pipeline {
-    agent {
-        docker {
-            image 'pipeline/jenkins'
-            args '-u 0:0 -v /var/run/docker.sock:/var/run/docker.sock'
-        }
-    }
+    agent any
     environment {
-        registry = "samarmatoussi/jcasc"
-        registryCredential = 'dockerHub'
-        dockerImage = ""
-    }                    
-    stages {
-        stage('git') {
-            steps {
-                echo 'Cloning the repository'
-                git branch: 'master', credentialsId: 'github-credentials', url: 'https://github.com/SamarMatoussi/JCasC.git'
-                sh 'docker run hello-world'
-            }
-        }
-        stage('unit_test') {
-            steps {
-                sh 'echo "Start the unit test"'
-            }
-        }
-        stage('Build Image') {
-            steps {
-                script {
-                    dockerImage = docker.build("${registry}:$BUILD_NUMBER")
-                }
-            }
-        }
-        stage('Deploy Image') {
-            steps {
-                script {
-                    docker.withRegistry('', registryCredential) {
-                        dockerImage.push()
-                    }
-                }
-            }
-        }
-     stage('Deploy to Production') {
-            steps {
-                script {
-                    docker.withRegistry('', registryCredential) {
-                        dockerImage.pull()
-                        docker.image("${registry}:$BUILD_NUMBER").run()
-                    }
-                }
-            }
-        }    
+        registry = "samarmatoussi/atlaslabs"
+        registryCredential = 'dockerhub-credentials'-
+        backendDockerImage = "samarmatoussi/atlaslabs-backend"
+        frontendDockerImage = "samarmatoussi/atlaslabs-frontend"
     }
-}''')
+
+    stages {
+        stage('CHECKOUT GIT') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'github-credentials', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
+                    git branch: 'main', url: "https://${GIT_USER}:${GIT_PASS}@github.com/SamarMatoussi/AtlasLabs.git"
+                }
+            }
+        }
+
+        stage('MAVEN CLEAN') {
+            steps {
+                dir('backend') {
+                    sh 'mvn clean'
+                }
+            }
+        }
+
+        stage('ARTIFACT CONSTRUCTION') {
+            steps {
+                dir('backend') {
+                    sh 'mvn package'
+                }
+            }
+        }
+
+        stage('SONARQUBE ANALYSIS') {
+            steps {
+                dir('backend') {
+                    withCredentials([usernamePassword(credentialsId: 'sonarqube-credentials', usernameVariable: 'SONAR_USER', passwordVariable: 'SONAR_PASS')]) {
+                        sh 'mvn sonar:sonar -Dsonar.login=$SONAR_USER -Dsonar.password=$SONAR_PASS'
+                    }
+                }
+            }
+        }
+
+        stage('RUN TESTS') {
+            steps {
+                dir('backend') {
+                    echo 'Launching Unit Tests...'
+                    sh 'mvn test'
+                }
+            }
+        }
+
+        stage('DOCKER BUILD BACKEND') {
+            steps {
+                script {
+                    docker.build("${backendDockerImage}:${BUILD_NUMBER}", "backend/").push()
+                }
+            }
+        }
+
+        stage('DOCKER BUILD FRONTEND') {
+            steps {
+                script {
+                    docker.build("${frontendDockerImage}:${BUILD_NUMBER}", "frontend/").push()
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            emailext(
+                subject: "Pipeline terminé avec succès !",
+                body: "Le pipeline a été exécuté avec succès.",
+                to: "matoussi.samar20@gmail.com"
+            )
+        }
+        failure {
+            emailext(
+                subject: "Échec du pipeline",
+                body: "Le pipeline a échoué. Veuillez vérifier les logs Jenkins.",
+                to: "matoussi.samar20@gmail.com"
+            )
+        }
+    }
+}
+''')
     }
   }
 }
